aa616398a4ebc850d703c4dfc89905f266534edf
Andrei Toterman <andrei.toterman@gmail.com>
2021-06-08 22:06:33 +0300

    changed some colors

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ fn main() {
     let (mut rl_handle, rl_thread) = raylib::init().size(1000, 1000).title("skeleton 2d").build();
 
     let mut args = std::env::args().skip(1);
-    let input = args.next().unwrap_or_else(|| String::from("points_2d.txt"));
+    let file_name = args.next().unwrap_or_else(|| String::from("points_2d.txt"));
 
     let mut points = Vec::new();
-    let input = BufReader::new(std::fs::File::open(&input).unwrap());
+    let input = BufReader::new(std::fs::File::open(&file_name).unwrap());
     for line in input.lines() {
         let line = line.unwrap();
         let mut tokens = line.split_whitespace().map(|token| token.parse().unwrap());
@@ -24,7 +24,7 @@ fn main() {
 
     let start = std::time::Instant::now();
     let mut triangles = delaunay(&points);
-    println!("delaunay took {} microseconds", start.elapsed().as_micros());
+    println!("{}", start.elapsed().as_micros());
 
     triangles.retain(|tri| point_inside_shape(&tri.centroid(), &points));
     let media_axis = medial_axis(&triangles);
@@ -76,27 +76,32 @@ fn main() {
         camera.zoom = (camera.zoom + rl_handle.get_mouse_wheel_move() * 0.1).max(0.1);
 
         let mut draw_handle = rl_handle.begin_drawing(&rl_thread);
-        draw_handle.clear_background(Color::RAYWHITE);
+        draw_handle.clear_background(Color::WHITE);
 
         {
             let mut draw_handle = draw_handle.begin_mode2D(camera);
 
             for &(v1, v2, v3) in triangles_draw.iter() {
-                draw_handle.draw_triangle(v1, v2, v3, Color::RED.fade(0.3));
+                draw_handle.draw_triangle(v1, v2, v3, Color::GREEN.fade(0.3));
                 if show_triangulation {
-                    draw_handle.draw_triangle_lines(v1, v2, v3, Color::RED);
+                    draw_handle.draw_line_ex(v1, v2, 1.0, Color::GREEN);
+                    draw_handle.draw_line_ex(v2, v3, 1.0, Color::GREEN);
+                    draw_handle.draw_line_ex(v3, v1, 1.0, Color::GREEN);
                 }
             }
-            draw_handle.draw_line_strip(&points_draw, Color::RED);
+
+            for v in points_draw.windows(2) {
+                draw_handle.draw_line_ex(v[0], v[1], 5.0, Color::BLACK);
+            }
 
             for &v in points_draw.iter() {
-                draw_handle.draw_circle_v(v, 2.0, Color::MAROON);
+                draw_handle.draw_circle_v(v, 6.0, Color::BLACK);
             }
 
             for &(v1, v2) in medial_axis_draw.iter() {
-                draw_handle.draw_line_ex(v1, v2, 1.5, Color::BLACK);
-                draw_handle.draw_circle_v(v1, 2.0, Color::LIGHTGRAY);
-                draw_handle.draw_circle_v(v2, 2.0, Color::LIGHTGRAY);
+                draw_handle.draw_line_ex(v1, v2, 5.0, Color::GREEN);
+                // draw_handle.draw_circle_v(v1, 6.0, Color::GREEN);
+                // draw_handle.draw_circle_v(v2, 6.0, Color::GREEN);
             }
         }
 
